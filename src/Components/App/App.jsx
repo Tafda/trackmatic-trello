f982c1c2b251d6f0da9cda981e760354d6664e33
Tafdav <tvurayayi@retrorabbit.co.za>
2019-07-11 05:49:59 +0200

    removed unused files

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Routes from '../../Shared/Routes';
 
 class App extends Component {
   render() {
     return (
-      
+      <Routes  />
     );
   }
 }
